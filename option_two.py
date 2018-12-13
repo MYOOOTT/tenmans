046f724f90313198a6ad44b324f4573ebe25bf21
MYOOOTT <mnguy1231@gmail.com>
2018-12-13 11:13:02 -0800

    Ten man basic implementation, shuffle + bug fixes

--- option_two.py
+++ option_two.py
@@ -33,15 +33,41 @@ async def tenmans(ctx, *, player: discord.Member=None):
             await ctx.send(str(player) + " has been added!")
             player_list.append(player)
         await ctx.send(str(10 - len(player_list)) + " spot(s) left")
+    if (10 - len(player_list) < 1):
+        await shuffle(ctx)
+
+@bot.command()
+async def shuffle(ctx): #might be good to check if lobby is full or not
+    str_players = await concatenize_players(player_list)
+    random.shuffle(str_players)
+    team_one = []
+    team_two = []
+    
+    for x in range(len(str_players)):
+        if x < 5:
+            team_one.append(str_players[x])
+        else:
+            team_two.append(str_players[x])
+
+    table = PrettyTable()
+    table.add_column("Team 1", team_one)
+    table.add_column("Team 2", team_two)
+    table.align = "c"
+
+    result = table.get_string(title="T E N M A N S")
+    print(result)
+    await ctx.send("Here are the teams\n```" + result + "```")
 
 @bot.command()
 async def showlist(ctx):
+    '''displays the lobby in a pretty table'''
     table = PrettyTable()
     table.add_column("Players", await concatenize_players(player_list))
     await ctx.send("```" + table.get_string() + "```")
 
 @bot.command()
 async def clear(ctx):
+    '''clears the lobby'''
     player_list.clear()
     await ctx.send("Clearing player lobby...")
 
@@ -55,10 +81,19 @@ async def check_length(ctx): #False = too many people
         return True
 
 async def concatenize_players(player_list):
+    '''converts Member models to strings'''
     string_list = []
     for player in player_list:
         string_list.append(str(player))
     return string_list
+
+async def check_list(ctx, member):
+    '''checks if player is in lobby already'''
+    for player in player_list:
+        if member == player:
+            await ctx.send("You're already in the lobby")
+            return True #true = is in lobby already
+    return False
 
 bot.run('NTIyNDU4Mzk2NjI5MjcwNTMx.DvLRJA.L1O1cqEIylU8WqVSA2EEDyl3htw')
 
