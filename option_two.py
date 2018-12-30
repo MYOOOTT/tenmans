79f7734332df0012d0b68f1dc54627699a853a37
MYOOOTT <mnguy1231@gmail.com>
2018-12-30 14:47:57 -0800

    I goofed

--- option_two.py
+++ option_two.py
@@ -215,7 +215,7 @@ async def reset(ctx):
 @bot.command()
 async def notifyme(ctx):
     '''The bot will @you when the lobby is full.'''
-    if (await lobby_exist(ctx)):
+    if (await git (ctx)):
         notify_list.append(ctx.author)
         await ctx.send("I'll let you know when the game is starting")
 
