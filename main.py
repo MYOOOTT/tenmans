c74e37ddc55a084abd8e7179bf30b7fe0b6ace73
MYOOOTT <mnguy1231@gmail.com>
2020-02-26 17:00:01 -0800

    Clearer logging

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from tenmans import Scrim
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='new_discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+handler = logging.FileHandler(filename='bot_logs.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s'))
 logger.addHandler(handler)
 
 bot = commands.Bot(command_prefix='?')
